38dc8ebde4c6562b7e380a7601216fda39b8c3f2
thanhvt <thanhvt@vissoft.vn>
2021-10-10 15:25:20 +0700

    init commit

---
+++ splash_inventory_service/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SplashInventoryServiceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'splash_inventory_service'
